a5d10bda5d02167a83afab3cfb6e0d93d4426291
P-H-B-D <98623538+P-H-B-D@users.noreply.github.com>
2023-08-15 01:35:06 -0700

    working MLP

--- jsgrad.js
+++ jsgrad.js
@@ -48,7 +48,7 @@ function drawDot(root) {
 
 
 class Val {
-    constructor(val, children = [], operation = null, label = null) {
+    constructor(val, children = [], operation = null, label = String(Math.floor(Math.random()*10000))) {
         this.val = val;
         this.prev = new Set(children);
         this.operation = operation;
@@ -94,7 +94,6 @@ class Val {
 
 
     backpropagate() {
-        
 
         //build topological order
         const nodes = new Set();
@@ -118,70 +117,153 @@ class Val {
 
 class Neuron{
     constructor(nInputs){
-        this.weights= Array.from({ length: nInputs }, () => new Val(Math.random() * 2 - 1, [], null, "w"));
+        this.weights= Array.from({ length: nInputs }, () => new Val(Math.random() * 2 - 1));
         this.bias= new Val (Math.random() * 2 - 1);
     }
     
     forward(inputs){
-        // console.log(this.weights[0].add(this.weights[1]))
-        let outArr=inputs.map((oldval,idx)=>{
-            let oldValObj= new Val(oldval, [], null, "w");
-            let act = this.weights[idx].multiply(oldValObj).add(this.bias);
+        if(typeof(inputs[0])== "number"){
+            this.inputs=inputs.map((oldval,idx)=>{
+                return new Val(oldval);
+            });
+        }
+        else{
+            this.inputs=inputs;
+        }
+        
+
+        let dotProduct=this.inputs.map((oldval,idx)=>{
+            let act = this.weights[idx].multiply(oldval);
             return act;
         });
 
-        var sum=new Val(0);
-        for (var i = 0; i < outArr.length; i++) {
-            sum = sum.add(outArr[i]);
-        }
+        dotProduct=dotProduct.reduce((a,b)=>a.add(b));
+        const out= dotProduct.add(this.bias).tanh();
 
-        sum = sum.tanh();
-        sum.backpropagate();
-        return sum;
-
-
+        return out;
+        
+    }
+    parameters(){
+        return this.weights.concat(this.bias);
     }
 }
 
-x=[2.0,3.0];
-let k = new Neuron(2);
-
-console.log(k.forward(x));
-
-
-
-
-
-// Forward pass
-const b=new Val(6.88137, [], null, "b");
-const x2= new Val(0.0, [], null, "x2");
-const w2= new Val(1.0, [], null, "w2");
-
-const x1= new Val(2.0, [], null, "x1");
-const w1= new Val(-3.0, [], null, "w1");
-
-const x2w2= x2.multiply(w2); x2w2.label= "x2*w2";
-const x1w1= x1.multiply(w1); x1w1.label= "x1*w1";
-const x1w1x2w2= x1w1.add(x2w2); x1w1x2w2.label= "x1w1+x2w2";
-const n=x1w1x2w2.add(b); n.label= "n";
-const o=n.tanh(); o.label= "o";
-
-
-o.backpropagate();
-
-
-
-const result = o;
-
-
-
-
-
-const graph = drawDot(result);
-graph.render('png', 'output.png', function(err) {
-    if (err) {
-        console.error("Error rendering graph:", err);
-    } else {
-        console.log("Graph rendered and saved to output.png");
+class Layer{
+    constructor(nInputs,nNeurons){
+        this.neurons=Array.from({ length: nNeurons }, () => new Neuron(nInputs));
     }
-});
+
+    forward(inputs){
+        let out=[];
+        for(let neuron of this.neurons){
+            out.push(neuron.forward(inputs));
+        }
+        return out;
+    }
+    parameters(){
+        let out=[];
+        for(let neuron of this.neurons){
+            out=out.concat(neuron.parameters());
+        }
+        return out;
+    }
+}
+
+class MLP{
+    constructor(nInputs,nHidden,nOutputs){
+        this.hidden= new Layer(nInputs,nHidden);
+        this.output= new Layer(nHidden,nOutputs);
+    }
+
+    forward(inputs){
+        let out= this.hidden.forward(inputs);
+        out= this.output.forward(out);
+        return out;
+    }
+    parameters(){
+        let out=[];
+        out=out.concat(this.hidden.parameters());
+        out=out.concat(this.output.parameters());
+        return out;
+    }
+
+}
+
+
+x= [[3.0,-5.2,7.1],
+    [1.0,2.0,3.0],
+    [2.0,4.0,0.0],
+    [1.0,1.0,1.0],
+];
+
+y= [1.0,-1.0,-1.0,-1.0];
+
+let net= new MLP(3,5,1);
+console.log("Parameters: " + net.parameters().length);
+
+// make predictions for each row
+for(let i=0;i<x.length;i++){
+    let row=x[i];
+    let pred= net.forward(row);
+}
+
+var loss= new Val(0.0);
+
+//calculate loss
+for(let i=0;i<x.length;i++){
+    let row=x[i];
+    let pred= net.forward(row);
+    let error= pred[0].add(new Val(-y[i]));
+    loss= loss.add(error.multiply(error));
+}
+loss.backpropagate();
+console.log("Loss: "+ loss.val)
+
+
+let stepSize= 0.01;
+for (i = 0; i < 50; i++) {
+    //move in the opposite direction of the gradient
+
+    for(let param of net.parameters()){
+        param.val -= stepSize * param.grad;
+    }
+
+    // make predictions for each row
+    for(let i=0;i<x.length;i++){
+        let row=x[i];
+        let pred= net.forward(row);
+    }
+    
+    var loss= new Val(0.0);
+
+    //calculate loss
+    for(let i=0;i<x.length;i++){
+        let row=x[i];
+        let pred= net.forward(row);
+        let error= pred[0].add(new Val(-y[i]));
+        loss= loss.add(error.multiply(error));
+    }
+    loss.backpropagate();
+    console.log("Loss: "+ loss.val)
+}
+
+// print the predictions
+for (i = 0; i < x.length; i++) {
+    let row = x[i];
+    let pred = net.forward(row);
+    console.log(pred[0].val, y[i]);
+}
+
+let pred = net.forward([1.0,2.0,3.0]);
+console.log(pred[0].val);
+
+
+
+// const graph = drawDot(loss);
+// graph.render('png', 'output.png', function(err) {
+//     if (err) {
+//         console.error("Error rendering graph:", err);
+//     } else {
+//         console.log("Graph rendered and saved to output.png");
+//     }
+// });
