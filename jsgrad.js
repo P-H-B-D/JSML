d0372f2e04e5be5fd0d87a949af8010d7641e5ea
P-H-B-D <98623538+P-H-B-D@users.noreply.github.com>
2023-08-15 23:46:57 -0700

    basic site integration

--- jsgrad.js
+++ jsgrad.js
@@ -1,53 +1,3 @@
-const graphviz = require('graphviz');
-
-function trace(root) {
-    let nodes = new Set();
-    let edges = new Set();
-
-    function build(v) {
-        if (!nodes.has(v)) {
-            nodes.add(v);
-            for (let child of v.prev) {
-                edges.add([child, v]);
-                build(child);
-            }
-        }
-    }
-
-    build(root);
-    return { nodes, edges };
-}
-
-function drawDot(root) {
-    const { nodes, edges } = trace(root);
-
-    let g = graphviz.digraph('G');
-    g.set('rankdir', 'LR');
-
-    nodes.forEach(n => {
-        let uid = n.label ? n.label : "null";
-        g.addNode(uid, {
-            label: `{ ${n.label ? n.label : "null"} | val ${n.val.toFixed(4)} | grad ${n.grad.toFixed(4)} }`,
-            shape: 'record'
-        });
-
-        if (n.operation) {
-            g.addNode(uid + n.operation, {
-                label: n.operation
-            });
-            g.addEdge(uid + n.operation, uid);
-        }
-    });
-
-    edges.forEach(([n1, n2]) => {
-        g.addEdge(n1.label ? n1.label : "null", (n2.label ? n2.label : "null") + n2.operation);
-    });
-
-    return g;
-}
-
-
-
 class Val {
     constructor(val, children = [], operation = null, label = String(Math.floor(Math.random() * 10000))) {
         this.val = val;
@@ -55,9 +5,7 @@ class Val {
         this.operation = operation;
         this.label = label;
         this.grad = 0.0;
-        this.backward = () => {
-            return null;
-        };
+        this.backward = () => null;
     }
 
     add(other) {
@@ -93,6 +41,19 @@ class Val {
 
     }
 
+    identity() {
+
+        let x = this.val;
+        const out = new Val(x, [this], "identity");
+        const backward = () => {
+            this.grad += 1.0 * out.grad;
+        }
+        out.backward = backward;
+        return out;
+
+    }
+
+    
 
     backpropagate() {
 
@@ -119,9 +80,10 @@ class Val {
 
 
 class Neuron {
-    constructor(nInputs) {
+    constructor(nInputs, activation='tanh') {
         this.weights = Array.from({ length: nInputs }, () => new Val(Math.random() * 2 - 1));
         this.bias = new Val(Math.random() * 2 - 1);
+        this.activation = activation;
     }
 
     forward(inputs) {
@@ -138,9 +100,22 @@ class Neuron {
             let act = this.weights[idx].multiply(oldval);
             return act;
         });
-
+        
         dotProduct = dotProduct.reduce((a, b) => a.add(b));
-        const out = dotProduct.add(this.bias).tanh();
+        const z = dotProduct.add(this.bias);
+        
+        let out;
+        switch (this.activation) {
+            case 'tanh':
+                out = z.tanh();
+                break;
+            case 'identity':
+                out = z.identity();
+                break;
+
+            default:
+                throw new Error('Unknown activation function: ' + this.activation);
+        }
         return out;
 
     }
@@ -150,8 +125,8 @@ class Neuron {
 }
 
 class Layer {
-    constructor(nInputs, nNeurons) {
-        this.neurons = Array.from({ length: nNeurons }, () => new Neuron(nInputs));
+    constructor(nInputs, nNeurons, activation='tanh') {
+        this.neurons = Array.from({ length: nNeurons }, () => new Neuron(nInputs, activation));
     }
 
     forward(inputs) {
@@ -171,100 +146,73 @@ class Layer {
 }
 
 class MLP {
-    constructor(nInputs, nHidden, nOutputs) {
-        this.hidden = new Layer(nInputs, nHidden);
-        this.output = new Layer(nHidden, nOutputs);
+    constructor(nInputs, hiddenLayers, nOutputs, activations) {
+        this.layers = [];
+
+        // Input validation: Check if the number of activations matches the number of layers
+        if (activations.length !== hiddenLayers.length + 1) {
+            throw new Error('Number of activations must be equal to the number of hidden layers plus 1 (for the output layer)');
+        }
+
+        let prevLayerNeuronCount = nInputs;
+
+        // Construct hidden layers
+        for (let i = 0; i < hiddenLayers.length; i++) {
+            let nNeurons = hiddenLayers[i];
+            let activation = activations[i];
+            this.layers.push(new Layer(prevLayerNeuronCount, nNeurons, activation));
+            prevLayerNeuronCount = nNeurons;
+        }
+
+        // Add output layer
+        let outputActivation = activations[activations.length - 1];
+        this.layers.push(new Layer(prevLayerNeuronCount, nOutputs, outputActivation));
     }
 
     forward(inputs) {
-        let out = this.hidden.forward(inputs);
-        out = this.output.forward(out);
-        return out;
+        let output = inputs;
+        for (let layer of this.layers) {
+            output = layer.forward(output);
+        }
+        return output;
     }
     parameters() {
-        let out = [];
-        out = out.concat(this.hidden.parameters());
-        out = out.concat(this.output.parameters());
-        return out;
+        let params = [];
+        for (let layer of this.layers) {
+            params = params.concat(layer.parameters());
+        }
+        return params;
     }
-
 }
 
+function trainModel(net, x, y, stepSize, epochs) {
+    for (let epoch = 0; epoch < epochs; epoch++) {
+        let loss = new Val(0.0);
 
-x = [[3.0, -5.2, 7.1],
-[1.0, 2.0, 3.0],
-[2.0, 4.0, 0.0],
-[1.0, 1.0, 1.0],
-];
+        for (let i = 0; i < x.length; i++) {
+            let row = x[i];
+            let pred = net.forward(row);
+            let error = pred[0].add(new Val(-y[i]));
+            loss = loss.add(error.multiply(error));
+        }
 
-y = [1.0, -1.0, -1.0, -1.0];
+        loss.backpropagate();
 
-let net = new MLP(3, 5, 1);
-console.log("Parameters: " + net.parameters().length);
+        for (let param of net.parameters()) {
+            param.val -= stepSize * param.grad;
+        }
 
-// make predictions for each row
-for (let i = 0; i < x.length; i++) {
-    let row = x[i];
-    let pred = net.forward(row);
-}
-
-var loss = new Val(0.0);
-
-//calculate loss
-for (let i = 0; i < x.length; i++) {
-    let row = x[i];
-    let pred = net.forward(row);
-    let error = pred[0].add(new Val(-y[i]));
-    loss = loss.add(error.multiply(error));
-}
-loss.backpropagate();
-console.log("Loss: " + loss.val)
-
-
-let stepSize = 0.01;
-for (i = 0; i < 50; i++) {
-    //move in the opposite direction of the gradient
-
-    for (let param of net.parameters()) {
-        param.val -= stepSize * param.grad;
+        console.log("Epoch:", epoch + 1, " Loss:", loss.val);
     }
+}
 
-    // make predictions for each row
+function printPredictions(net, x, y) {
     for (let i = 0; i < x.length; i++) {
         let row = x[i];
         let pred = net.forward(row);
+        console.log("Prediction:", pred[0].val, "Actual:", y[i]);
     }
-
-    var loss = new Val(0.0);
-
-    //calculate loss
-    for (let i = 0; i < x.length; i++) {
-        let row = x[i];
-        let pred = net.forward(row);
-        let error = pred[0].add(new Val(-y[i]));
-        loss = loss.add(error.multiply(error));
-    }
-    loss.backpropagate();
-    console.log("Loss: " + loss.val)
 }
 
-// print the predictions
-for (i = 0; i < x.length; i++) {
-    let row = x[i];
-    let pred = net.forward(row);
-    console.log(pred[0].val, y[i]);
-}
-
-let pred = net.forward([1.0, 2.0, 3.0]);
-console.log(pred[0].val);
-
-
-
-// const graph = drawDot(loss);
-// graph.render('png', 'output.png', function(err) {
-//     if (err) {
-//         console.error("Error rendering graph:", err);
-//     } else {
-//         console.log("Graph rendered and saved to output.png");
-//     }
-// });
+// Exporting classes and functions to be accessible in other files
+// export { Val, Neuron, Layer, MLP, trainModel, printPredictions };
---
+++ script.js
@@ -0,0 +1,37 @@
+function runMLScript() {
+    // Generate Sample Data
+    x=[[0,0],[0,1],[1,0],[1,1]];
+    y=[0,1,1,0];
+    
+    // MLP Configuration
+    const input_size = 2;
+    const hidden_layers = [10]; // One hidden layer with 10 neurons
+    const output_size = 1;
+    const activations = ['tanh','tanh']; // 'tanh' for hidden layers, 'identity' for output layer
+    
+    // Create the MLP
+    let net = new MLP(input_size, hidden_layers, output_size, activations);
+    console.log("Parameters: " + net.parameters().length);
+    
+    // Training Configuration
+    const stepSize = 0.01;
+    const epochs = 100;
+    
+    // Train the Model
+    trainModel(net, x, y, stepSize, epochs);
+    
+    // Print Predictions
+    printPredictions(net, x, y);
+    
+    // Test Prediction
+    let pred = net.forward([0.5, 0.3]);
+    console.log("Train Network:", pred[0].val);
+    
+    // Display the prediction on the HTML page
+    const predictionElement = document.getElementById("prediction");
+    predictionElement.textContent = "Test Prediction: " + pred[0].val;
+}
+
+// Attach the runMLScript function to the button click event
+const runButton = document.getElementById("runButton");
+runButton.addEventListener("click", runMLScript);
